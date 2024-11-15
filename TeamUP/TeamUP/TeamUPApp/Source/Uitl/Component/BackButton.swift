d192efbaf2968140ad2e17b64def65fc6edb7b33
Soomin Kim <140694831+sumchive@users.noreply.github.com>
2024-11-15 15:43:57 +0900

    [add] : Extension, Component 추가

---
+++ TeamUP/TeamUP/TeamUPApp/Source/Uitl/Component/BackButton.swift
@@ -0,0 +1,24 @@
+//
+//  BackButton.swift
+//  TeamUP
+//
+//  Created by 김수민 on 11/15/24.
+//
+
+import SwiftUI
+
+struct BackButton: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+        }) {
+            HStack {
+                Image(systemName: "chevron.left")
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundStyle(.black)
+            }
+        }
+    }
+}
