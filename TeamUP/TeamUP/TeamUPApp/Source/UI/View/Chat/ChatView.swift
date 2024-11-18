8aba9c934d0f7e36900b1b5a43dc476084e7d924
sueun kim <doms@kakao.com>
2024-11-18 13:56:42 +0900

    [feat]: 채팅 분류 뷰 구현

--- TeamUP/TeamUP/TeamUPApp/Source/UI/View/Chat/ChatView.swift
+++ TeamUP/TeamUP/TeamUPApp/Source/UI/View/Chat/ChatView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct ChatView: View {
+    enum ChatTab: Int, CaseIterable {
+        case privateChat
+        case groupChat
+    }
+    @State private var selectedTab: ChatTab = .privateChat
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            chatSpliteView()
+            switch selectedTab {
+            case .privateChat:
+                Text("개인 채팅 뷰")
+            case .groupChat:
+                GroupChatView()
+            }
+            Spacer()
+        }
+    }
+    func chatSpliteView() -> some View {
+        HStack (alignment: .bottom,spacing: 0){
+            Button {
+                selectedTab = .groupChat
+            }label: {
+                VStack (spacing: 8) {
+                    Text("그룹")
+                        .frame(maxWidth: .infinity)
+                    Rectangle()
+                        .fill(selectedTab == .groupChat ? Color.customBlue : Color.gray)
+                        .frame(height: selectedTab == .groupChat ? 3 : 0.4)
+                }
+            }
+            Button {
+                selectedTab = .privateChat
+            }label: {
+                VStack {
+                    Text("개인")
+                        .frame(maxWidth: .infinity)
+                    Rectangle()
+                        .fill(selectedTab == .privateChat ? Color.customBlue : Color.gray)
+                        .frame(height:selectedTab == .privateChat ? 3 : 0.4)
+                }
+            }
+        }
+        .font(Font.semibold20)
+        .foregroundStyle(.black)
+        .padding(.horizontal, 16)
     }
 }
 
---
+++ TeamUP/TeamUP/TeamUPApp/Source/UI/View/Chat/GroupChatView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupChatView.swift
+//  TeamUP
+//
+//  Created by Soom on 11/18/24.
+//
+
+import SwiftUI
+
+struct GroupChatView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    GroupChatView()
+}
