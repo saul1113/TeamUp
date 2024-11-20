30319b1d3653dd8578e49301dd2d0d3e677909d0
Soomin Kim <140694831+sumchive@users.noreply.github.com>
2024-11-20 16:30:25 +0900

    [design] : HomeRounge 디자인 수정

--- TeamUP/TeamUP/TeamUPApp/Source/UI/View/Lounge/LoungeView.swift
+++ TeamUP/TeamUP/TeamUPApp/Source/UI/View/Lounge/LoungeView.swift
@@ -86,63 +86,68 @@ struct LoungeView: View {
     var body: some View {
       
             VStack(alignment: .leading) {
-                CustomSearchBar(searchText: $searchText, placeholder: "검색어를 입력해주세요 (ex 웹프로젝트, 알고리즘 스터디", onSearch: { text in
+                CustomSearchBar(searchText: $searchText, placeholder: "검색어를 입력해주세요", onSearch: { text in
                     print("검색실행")
                 })
                 .padding(.horizontal)
                 
-                // 스터디 카테고리 필터링
-                HStack(spacing: 5) {
+
+                HStack (spacing: 20){
                     ForEach([nil] + RoungeCategory.allCases, id: \.self) { category in
                         Button(action: {
                             selectedCategory = category
                         }) {
                             VStack(spacing: 2){
                                 Text(category?.rawValue ?? "전체")
-                                    .font(.system(size: 16, weight: .bold))
+                                    .font(.semibold20)
                                     .foregroundColor(selectedCategory == category ? .black : .gray)
                                 if selectedCategory == category {
                                     Rectangle()
                                         .frame(width: 30, height: 2)
-                                        .foregroundColor(.blue)
+                                        .foregroundColor(.customBlue)
                                         .animation(.easeInOut, value: selectedCategory)
                                 }
                             }
-                            .padding(.horizontal)
-                            .padding(.top, 3)
-                            .padding(.bottom, 10)
                         }
                     }
                     Spacer() // 버튼들을 왼쪽으로 밀기
                 }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 20)
                 
                 // 최신순 저장많은순 조회순 필터링
-                HStack {
+                HStack (spacing: 15){
                     Button(action: {
                         selectedSort = "최신순"
                     }) {
                         Text("• 최신순")
-                            .foregroundColor(selectedSort == "최신순" ? .blue : .gray)
+                            .foregroundColor(selectedSort == "최신순" ? .customBlue : .gray)
+                            .font(.medium14)
                     }
                     
                     Button(action: {
                         selectedSort = "저장많은순"
                     }) {
                         Text("• 저장많은순")
-                            .foregroundColor(selectedSort == "저장많은순" ? .blue : .gray)
+                            .foregroundColor(selectedSort == "저장많은순" ? .customBlue : .gray)
+                            .font(.medium14)
                     }
                     
                     Button(action: {
                         selectedSort = "조회순"
                     }) {
                         Text("• 조회순")
-                            .foregroundColor(selectedSort == "조회순" ? .blue : .gray)
+                            .foregroundColor(selectedSort == "조회순" ? .customBlue : .gray)
+                            .font(.medium14)
                     }
                 }
                 .padding(.horizontal)
                 .font(.caption)
                 .padding(.bottom, 10)
+                
+                
                 Divider()
+                
                 List {
                     ForEach(sortedItems.filter { rounge in
                         if let selectedCategory = selectedCategory {
